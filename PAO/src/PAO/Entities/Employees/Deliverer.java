be47232d8311db242180fbf325207804c0228fec
teodormititelu <72453179+teodormititelu@users.noreply.github.com>
2022-05-23 03:50:32 +0300

    Delete PAO directory

--- PAO/src/PAO/Entities/Employees/Deliverer.java
+++
@@ -1,9 +0,0 @@
-package PAO.Entities.Employees;
-
-import java.time.LocalDate;
-
-public class Deliverer extends Employee{
-    public Deliverer(String firstName, String lastName, LocalDate birthDate) {
-        super(firstName, lastName, birthDate);
-    }
-}
--- PAO/src/PAO/Entities/Employees/Waiter.java
+++
@@ -1,9 +0,0 @@
-package PAO.Entities.Employees;
-
-import java.time.LocalDate;
-
-public class Waiter extends Employee{
-    public Waiter(String firstName, String lastName, LocalDate birthDate) {
-        super(firstName, lastName, birthDate);
-    }
-}
